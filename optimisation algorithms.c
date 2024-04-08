dce16e30ffd88e04e00ad7f8e4371f340513de21
amen allah kalai <148721192+amenallahkalai@users.noreply.github.com>
2024-04-08 22:39:20 +0100

    Update optimisation algorithms.c

--- optimisation algorithms.c
+++ optimisation algorithms.c
@@ -12,3 +12,10 @@ float derivative_logLoss(float y,float a,float x){
     return derivative_loss(y,a) * derivative_sigmoid(a) * x;
 }
 
+float vect(float* w,float* x){
+    float vect = 0;
+    for (int i = 0;i<2;i++){
+        vect += *(x+i) * *(w+i);
+    }
+    return vect;
+}
